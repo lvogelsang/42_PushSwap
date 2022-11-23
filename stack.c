b1c7f6772ee0f56a5ff6f5f1160e07088f1b5e2b
Lena Vogelsang <lvogelsa@F3C015.42adel.org.au>
2022-11-23 15:09:34 +1030

    normalize stack a

--- stack.c
+++ stack.c
@@ -6,7 +6,7 @@
 /*   By: lvogelsa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 13:13:18 by lvogelsa          #+#    #+#             */
-/*   Updated: 2022/11/23 14:55:32 by lvogelsa         ###   ########.fr       */
+/*   Updated: 2022/11/23 15:09:09 by lvogelsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,7 +98,6 @@ void	normalize_stack_a(char **stack_a, char **stack_a_index, char **stack_a_redu
 		}
 		i++;
 	}
-//	normalize_stack_a_2(stack_a, stack_a_copy, len, min);
 }
 
 void	stack_remove_min(char **stack_a_reduced, int min)
@@ -117,61 +116,6 @@ void	stack_remove_min(char **stack_a_reduced, int min)
 	}
 	stack_a_reduced[i] = "x";
 }
-// move forward adn equal the small numbe to x;
-
-/*
-void	normalize_stack_a_2(char **stack_a, char **stack_a_copy, int len, int min)
-{
-	int	i;
-	int	index;
-	int	j;
-	int	num;
-	int	x;
-	
-	i = 1;
-	index = 0;
-	while (i < len)
-	{
-//		printf("%d\n+\n", min);
-		index = 0;
-		num = ft_atoi(stack_a_copy[0]);
-		min += 1;
-		j = 0;
-		while (j < len)
-		{
-			if (ft_atoi(stack_a_copy[j]) > min && num > ft_atoi(stack_a_copy[j]))
-			{
-				num = ft_atoi(stack_a_copy[j]);
-				index = j;
-			}
-			j++;
-		}
-		min = ft_atoi(stack_a_copy[index]);
-//		printf("%d\n+\n", min);
-		stack_a[index] = ft_itoa(i);
-		i++;
-		//-------------------------
-		x = 0;
-		while (x < stack_length(stack_a))
-		{
-			printf("%s\n", stack_a[x]);
-	//	printf("%s\n", stack_a_copy[i]);
-			x++;
-		}
-		printf("%s\n", "-");
-	//-------------------------
-	}
-	//-------------------------
-	x = 0;
-	while (x < stack_length(stack_a))
-	{
-		printf("%s\n", stack_a[x]);
-	//	printf("%s\n", stack_a_copy[i]);
-		x++;
-	}
-	printf("%s\n", "-");
-	//-------------------------
-}*/
 
 void	create_stack_b(int argc, char **stack_b)
 {
