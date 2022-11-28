c53086fdcd65d5312275672ca026381ee308a6f0
Lena Vogelsang <lenav@Lenas-MacBook-Pro.local>
2022-11-28 16:35:07 +1030

    steps 12,551

--- sort_buckets.c
+++ sort_buckets.c
@@ -6,7 +6,7 @@
 /*   By: lvogelsa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:55:23 by lvogelsa          #+#    #+#             */
-/*   Updated: 2022/11/28 16:31:01 by lvogelsa         ###   ########.fr       */
+/*   Updated: 2022/11/28 16:34:21 by lvogelsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	sort_buckets(char **stack_a, char **stack_b, int bucket_count)
 
 	steps = 0;
 		j = 0;
+		x = 0;
 		while (j < BUCKET_SIZE)
 		{
-			x = 0;
 			while (ft_atoi(stack_b[0]) != stack_max_value(stack_b))
 			{
 				push_swap_b(stack_b, RB);
@@ -33,7 +33,7 @@ void	sort_buckets(char **stack_a, char **stack_b, int bucket_count)
 			push_a(stack_b, stack_a);
 			steps++;
 			// can imporvwe by checking if I pass the next max (need to keep x in mind though!!!)
-			while (x > 0)
+			while (x > 0 && (ft_atoi(stack_b[0]) != stack_max_value(stack_b))) 
 			{
 				push_swap_b(stack_b, RRB);
 				x--;
